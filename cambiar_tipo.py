9ca622210a1f5c3e7ebcd35cfe76790ba3409a88
IvanSerranoGit <contacto.ivanserrano@gmail.com>
2021-04-03 18:22:43 -0600

    Se sube primera parte del curso

---
+++ cambiar_tipo.py
@@ -0,0 +1,21 @@
+numero1 = input ("Escribe un núumero: ")
+
+numero2 = input ("Escribe un número: ")
+
+print(numero1 + numero2)
+
+numero1 = int(numero1)
+numero2 = int(numero2)
+
+print(numero1 + numero2)
+
+numero_decimal = 4.5
+int(4.5)
+
+print(numero_decimal)
+
+str(numero_decimal)
+
+print(numero_decimal)
+
+
